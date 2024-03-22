743f64a24b27b1753c489d193b6dc5bbc277f6bf
mucandev <mucandev@gmail.com>
2024-03-22 14:36:30 +0100

    ajout widget et zone widget personnalisé

--- planty/wp-content/themes/planty/functions.php
+++ planty/wp-content/themes/planty/functions.php
@@ -3,14 +3,54 @@
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
-    // Chargement du style.css du thème parent 
+    // Chargement du style.css du thème parent GeneratePress
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     // Chargement du theme.css du thème enfant planty
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+    // Chargement du /css/widgets/image-titre-widget.css pour widget vignette
+    wp_enqueue_style('image-titre-widget', get_stylesheet_directory_uri() . '/css/image-titre-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/image-titre-widget.css'));
 }
+    
+
+/* CHARGEMENT DES WIDGETS */
+require_once(__DIR__ . '/widgets/ImageTitreWidget.php');
+
+function register_widgets()
+{
+    //On enregistre le widget avec la class Image_Titre_Widget
+    register_widget('Image_Titre_Widget');
+}
+//On demander à wordpress de charger des widget selon la fonction register_widgets()
+add_action('widgets_init', 'register_widgets');
 
 
-/*chargement des widgets*/
+/* NOUVELLE ZONE DE WIDGETS */
+function main_widgets_init() {
+ 
+    register_sidebar( array(
+   
+    'name' => __('zone de widget main','text-domain'),
+    'id' => 'main-widget-area',
+    'description'   => __( 'Zone de widget pour le thème enfant', 'text-domain' ),
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<div class="mwa-title">',
+    'after_title' => '</div>',
+    ) );
+   }
+   
+   add_action( 'widgets_init', 'main_widgets_init' );
+
+
+
+
+
+
+// Gestion des menus
+// function enregistre_mon_menu() {
+//     register_nav_menu( 'menu_header', __( 'Menu header' ) );
+// }
+// add_action( 'init', 'enregistre_mon_menu' );
 
 
 /*SHORTCODES*/
---
+++ planty/wp-content/themes/planty/header.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying the header.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php generate_do_microdata( 'body' ); ?>>
+	<?php
+	/**
+	 * wp_body_open hook.
+	 * @since 2.3
+	 */
+	do_action( 'wp_body_open' ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- core WP hook.
+	?>
+
+ <!-- ajout pour planty 
+
+    <?php 	wp_nav_menu(array( 'theme_location' => 'menu_header' )) 	?>
+
+end pour planty -->
+
+	<?php
+	
+	/**
+	 * generate_header hook.	 *
+	 * @since 1.3.42	 *
+	 * @hooked generate_construct_header - 10
+	 */
+	do_action( 'generate_header' );
+	?>
+
+	<div <?php generate_do_attr( 'page' ); ?>>
+		<?php
+		/**
+		 * generate_inside_site_container hook.
+		 *		 * @since 2.4
+		 */
+		do_action( 'generate_inside_site_container' );
+		?>
+		<div <?php generate_do_attr( 'site-content' ); ?>>
+			<?php
+			/**
+			 * generate_inside_container hook.
+			 * @since 0.1
+			 */
+			do_action( 'generate_inside_container' );
